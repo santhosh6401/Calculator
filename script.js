e7efed0f0f5e0f039cc721093922d36e85b560f8
santhosh6401 <87711860+santhosh6401@users.noreply.github.com>
2021-07-21 21:13:13 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+		function insert(num){
+			document.form.textview.value=document.form.textview.value+num;
+		}
+
+		function equal(){
+			var exp = document.form.textview.value;
+			if(exp){
+				document.form.textview.value= eval(exp)
+			}
+		
+
+		}
+		function clean(){
+			document.form.textview.value="";
+		} 
+		function back(){
+			var num=document.form.textview.value;
+			document.form.textview.value=num.substring(0,num.length-1);
+		}
